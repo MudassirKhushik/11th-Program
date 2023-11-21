6adc6677db33d731dc3ac1c5b40e1f84332051cd
Mudassir Khushik <150449494+MudassirKhushik@users.noreply.github.com>
2023-11-21 19:16:17 +0500

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -1,5 +1,20 @@
 #include<iostream>
 using namespace std;
 int main(){
-  
+  int num1, num2;
+	cout << "\tEnter Two Numbers : " << endl;
+	cout << "First number : "; 
+	cin >> num1;
+	cout << "Second number : ";
+	cin >> num2;
+	
+	if (num1 == num2) {
+		cout << "The entered both numbers are equal.";
+	} 
+	
+	else {
+		cout << "The entered both numbers sre not equal. ";
+	}
+	
+	return 0;
 }
